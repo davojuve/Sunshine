58140c61c8c5cad22f622058bfb0bc7e2bf1098f
DavitWork <DavitWork>
2016-05-13 16:36:23 +0400

    4a-31 converse

--- app/src/androidTest/java/am/wedo/sunshine/ApplicationTest.java
+++ app/src/androidTest/java/am/wedo/sunshine/ApplicationTest.java
@@ -1,7 +1,14 @@
 package am.wedo.sunshine;
 
 import android.app.Application;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+
+import am.wedo.sunshine.data.WeatherContract;
+import am.wedo.sunshine.data.WeatherDbHelper;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -10,4 +17,124 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void testCreateDb() throws Throwable{
+        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+        SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+        db.close();
+    }
+
+    public void testInsertReadDb(){
+        // test data insert to db
+        String testName = "North Pole";
+        String testLocationSetting = "997545";
+        double testLatitude = 64.752;
+        double testLongitude = -145.256;
+
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // create a new map of values
+        ContentValues values = new ContentValues();
+        values.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(WeatherContract.LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
+
+
+        long locationRowId;
+        locationRowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+
+        // verify we got a row back.
+        assertTrue(locationRowId != -1);
+        Log.d("test", "New row id: " + locationRowId);
+
+        // Specify which columns you want
+        String[] columns = {
+                WeatherContract.LocationEntry._ID,
+                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+                WeatherContract.LocationEntry.COLUMN_CITY_NAME,
+                WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+                WeatherContract.LocationEntry.COLUMN_COORD_LONG
+        };
+
+        // A cursor is your primary interface to the query results
+        Cursor cursor = db.query(
+                    WeatherContract.LocationEntry.TABLE_NAME,
+                    columns,
+                    null, // WHERE
+                    null, // values for WHERE
+                    null, // group by
+                    null, // filter by
+                    null  // sort order
+                );
+
+        if(cursor.moveToFirst()){
+            // Get the value in each column by finding the appropriate column index.
+            int locationIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING);
+            String location = cursor.getString(locationIndex);
+
+            int nameIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_CITY_NAME);
+            String name = cursor.getString(nameIndex);
+
+            int latIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LAT);
+            Double latitude = cursor.getDouble(latIndex);
+
+            int longIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LONG);
+            Double longitude = cursor.getDouble(longIndex);
+
+            assertEquals(testName, name);
+            assertEquals(testLocationSetting, location);
+            assertEquals(testLatitude, latitude);
+            assertEquals(testLongitude, longitude);
+
+
+            ContentValues weatherValues = new ContentValues();
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DATETEXT, "20141205");
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, 1.1);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 1.2);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1.3);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, 75);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, 65);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+            long weatherRowId;
+            weatherRowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, weatherValues);
+            assertTrue(weatherRowId != -1);
+
+            Cursor weatherCursor = db.query(
+                    WeatherContract.WeatherEntry.TABLE_NAME,
+                    null, // just returns all columns
+                    null, // WHERE
+                    null, // values for WHERE
+                    null, // group by
+                    null, // filter by
+                    null  // sort order
+            );
+
+            if(weatherCursor.moveToFirst()) {
+                // Get the value in each column by finding the appropriate column index.
+                int weatherIndex = weatherCursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_LOC_KEY);
+                String location = cursor.getString(locationIndex);
+
+                int nameIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_CITY_NAME);
+                String name = cursor.getString(nameIndex);
+
+                int latIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LAT);
+                Double latitude = cursor.getDouble(latIndex);
+            }
+
+        }else{
+            fail("No values returned :(");
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/am/wedo/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/am/wedo/sunshine/data/WeatherDbHelper.java
@@ -20,23 +20,31 @@ public class WeatherDbHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        // TODO: 12-May-16 create location table
+        final String SQL_CREATE_LOCATION_TABLE =
+                "CREATE TABLE " + WeatherContract.LocationEntry.TABLE_NAME + "(" +
+                        WeatherContract.LocationEntry._ID + " INTEGER NOT NULL PRIMARY KEY, " +
+                        WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                        WeatherContract.LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                        WeatherContract.LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                        WeatherContract.LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL, " +
+                        " UNIQUE (" + WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING + ") " +
+                        " ON CONFLICT IGNORE);";
 
         final String SQL_CREATE_WEATHER_TABLE =
                 "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + "(" +
-                        WeatherContract.WeatherEntry._ID + "INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-                        WeatherContract.WeatherEntry.COLUMN_LOC_KEY + "INTEGER NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_DATETEXT + "TEXT NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_SHORT_DESC + "TEXT NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + "INTEGER NOT NULL, " +
+                        WeatherContract.WeatherEntry._ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherContract.WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
 
-                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + "REAL NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
 
-                        WeatherContract.WeatherEntry.COLUMN_HUMIDITY + "REAL NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_PRESSURE + "REAL NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + "REAL NOT NULL, " +
-                        WeatherContract.WeatherEntry.COLUMN_DEGREES + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
 
                         // Set up the location column as a foreign key to location table
                         " FOREIGN KEY (" + WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
@@ -45,11 +53,18 @@ public class WeatherDbHelper extends SQLiteOpenHelper{
                         // per location, it's created a UNIQUE constraint with REPLACE strategy
                         " UNIQUE (" + WeatherContract.WeatherEntry.COLUMN_DATETEXT + ", " +
                         WeatherContract.WeatherEntry.COLUMN_DATETEXT +") ON CONFLICT REPLACE);";
+//        Log.e("test", SQL_CREATE_LOCATION_TABLE);
+//        Log.e("test", SQL_CREATE_WEATHER_TABLE);
 
+        db.execSQL(SQL_CREATE_LOCATION_TABLE);
+        db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.LocationEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + WeatherContract.WeatherEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
