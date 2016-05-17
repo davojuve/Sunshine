f5284090ec2f26ed17fa2874696c859520b5d2c0
Davit Petrosyan <davitp27@gmail.com>
2016-05-17 21:24:24 +0400

    4b 06

--- app/src/main/java/am/wedo/sunshine/data/WeatherContract.java
+++ app/src/main/java/am/wedo/sunshine/data/WeatherContract.java
@@ -1,14 +1,31 @@
 package am.wedo.sunshine.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import java.net.URI;
+
 
 /**
  * Created by Davit: Store Database columns
  */
 public class WeatherContract {
 
+    public static final String CONTENT_AUTHORITY = "am.wedo.sunshine.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // possible paths
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
+
+
     /** Inner class that defines the table content of the location table */
     public static final class LocationEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
         // table name
         public static final String TABLE_NAME = "location";
@@ -29,6 +46,10 @@ public class WeatherContract {
 
     /** Inner class that defines the table content of the weather table */
     public static final class WeatherEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static final String TABLE_NAME = "weather";
 
@@ -60,6 +81,35 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south). Stored as a float
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(Long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String locationSetting, String startDate){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, String date){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendPath(date).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri){
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri){
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }
     }
 
 }
